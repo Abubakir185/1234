2e30523233878823afb198f6c03dad18b78250f3
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2024-09-16 21:14:41 +0500

    done

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+password = "salom1234"
+son = input("Password: ")
+
+
+while son != password:
+   son = input("try again: ")
+   if son == password:
+        print("You are welcome!")
+        break
+
+ 
